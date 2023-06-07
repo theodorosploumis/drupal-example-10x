525f9b83c26bcc38ba4ce7f1c17d8f45d7bfaad0
Theodoros Ploumis <me@theodorosploumis.com>
2023-06-07 12:02:08 +0300

    Remove excluded folders from git

--- web/modules/contrib/automatic_updates/tests/themes/automatic_updates_theme_with_updates/automatic_updates_theme_with_updates.post_update.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @file
- * Blank .post_update file.
- *
- * @see \Drupal\Tests\automatic_updates\Kernel\StatusCheck\StagedDatabaseUpdateValidatorTest
- */
--- web/modules/contrib/sdc/tests/modules/sdc_test/components/my-cta/my-cta.js
+++
@@ -1,8 +0,0 @@
-(function (Drupal, once) {
-  Drupal.behaviors.cta = {
-    attach: function attach(context) {
-      const [cta] = once('sdc--my-cta', '.sdc--my-cta', context);
-      console.log(cta);
-    },
-  };
-})(Drupal, once);
