ebc51ea4e1a7d8167ab663954b59b0e435382535
Theodoros Ploumis <me@theodorosploumis.com>
2023-06-08 18:47:29 +0300

    Prepare www for automated_updates module

--- assets/app/src/app.js
+++
@@ -1,6 +0,0 @@
-(function (document) {
-  var el = document.querySelector('#ssr-root');
-  if (el !== null) {
-    el.innerHTML = el.innerHTML + '<h2>... and I got appended on the client-side.</h2>'
-  }
-})(window.document);
--- assets/app/src/app.ssr.js
+++
@@ -1,6 +0,0 @@
-(function (DrupalServer, content) {
-  // DrupalServer object manipulation will update the document content
-  // for the current response.
-  let bodyTag = content.match(/\<body[^\>]*\>/i)[0];
-  DrupalServer.content = content.replace(bodyTag, bodyTag + '<div id="ssr-root"><h1>Say hello to server-side rendered entrypoints.</h1></div>');
-})(DrupalServer, DrupalServer.content);
